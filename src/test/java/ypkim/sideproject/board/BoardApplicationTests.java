e949558d0ce44dc71102d5e98a90d2fc746a890b
ypkim <ypkim0923@gmail.com>
2022-11-16 22:49:35 +0900

    #4 - SpringBoot initializing
    Spring Web
    Actuator
    dev-tools
    lombok

---
+++ src/test/java/ypkim/sideproject/board/BoardApplicationTests.java
@@ -0,0 +1,13 @@
+package ypkim.sideproject.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
