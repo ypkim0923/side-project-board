6c785254e53687867f166dcc6be0081740cea483
ypkim <ypkim0923@gmail.com>
2023-01-05 08:23:03 +0900

    #1 - Querydsl 지원을 이용하여 api 검색 옵션 추가

--- src/main/java/ypkim/sideproject/board/repository/ArticleCommentRepository.java
+++ src/main/java/ypkim/sideproject/board/repository/ArticleCommentRepository.java
@@ -1,11 +1,29 @@
 package ypkim.sideproject.board.repository;
 
+import com.querydsl.core.types.dsl.DateTimeExpression;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ypkim.sideproject.board.domain.ArticleComment;
+import ypkim.sideproject.board.domain.QArticle;
+import ypkim.sideproject.board.domain.QArticleComment;
 
 
 // JpaRepository 를 extends하면 @Repository 붙어있어서 붙일 필요 없음
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){
+        bindings.excludeUnlistedProperties(true);
+        bindings.including(root.createdAt, root.createdBy, root.content);
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdAt).first(DateTimeExpression::eq);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+    }
 }
