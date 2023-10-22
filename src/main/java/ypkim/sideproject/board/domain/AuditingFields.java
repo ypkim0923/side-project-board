e1985d9fd34bf119e04b845f96d22da033a9a41c
ypkim <ypkim0923@gmail.com>
2023-10-22 21:28:35 +0900

    직접사용하지 않는 클래스이고 상속만 당하는 클래스라서 abstract 추가

--- src/main/java/ypkim/sideproject/board/domain/AuditingFields.java
+++ src/main/java/ypkim/sideproject/board/domain/AuditingFields.java
@@ -1,7 +1,14 @@
 package ypkim.sideproject.board.domain;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
 import lombok.Getter;
 import lombok.ToString;
+
 import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
@@ -9,34 +16,29 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import java.time.LocalDateTime;
-
 @Getter
 @ToString
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
-public class AuditingFields {
+public abstract class AuditingFields {
 
-    // 아래 필드는 자동으로 설정되어야하는 값들이라 Setter를 설정하지 않음
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    @CreatedDate
-    @Column(nullable = false, updatable = false)
-    private LocalDateTime createdAt;
+	// 아래 필드는 자동으로 설정되어야하는 값들이라 Setter를 설정하지 않음
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	@CreatedDate
+	@Column(nullable = false, updatable = false)
+	private LocalDateTime createdAt;
 
-    @CreatedBy
-    @Column(nullable = false, updatable = false, length = 100)
-    private String createdBy;
+	@CreatedBy
+	@Column(nullable = false, updatable = false, length = 100)
+	private String createdBy;
 
-    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
-    @LastModifiedDate
-    @Column(nullable = false)
-    private LocalDateTime modifiedAt;
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	@LastModifiedDate
+	@Column(nullable = false)
+	private LocalDateTime modifiedAt;
 
-    @LastModifiedBy
-    @Column(nullable = false, length = 100)
-    private String modifiedBy;
+	@LastModifiedBy
+	@Column(nullable = false, length = 100)
+	private String modifiedBy;
 
 }
